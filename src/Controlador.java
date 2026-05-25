c6b3974f83ee68e1da1656c2f68d1b337dca4cfd
Baxter18 <matiasenriquez2007gmail.com>
2026-05-24 23:38:50 -0500

    Creacion menu administrativo con agregar y eliminar productos

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,4 +1,7 @@
 import JhonatanMetodos.ValidadorUsuarios;
+import MatiasMetodos.Inventario;
+import PedroMetodos.gestionStock;
+import PedroMetodos.mostrarInventario;
 import java.util.Scanner;
 
 public class Controlador {
@@ -17,6 +20,7 @@ public class Controlador {
 
         if (estadoUsuario==14){
             System.out.println("Bienvenido al panel de administrador");
+
         }else if(estadoUsuario==15){
             System.out.println("Bienvenido al Panel de clientes");
         }else{
@@ -24,5 +28,52 @@ public class Controlador {
         }
         
         System.out.println("----------------------------------------");
+    }
+
+    private void menuAdministrador(Scanner teclado){
+        int MAX_PRODUCTOS = 50;
+        gestionStock baseDeDatos = new gestionStock(MAX_PRODUCTOS);
+        Inventario MatiasMetodos = new Inventario();
+    
+        boolean salir = false;
+        while(!salir){
+            System.out.println("\n--MENU DE GESTION DE INVENTARIO--\n");
+            System.out.println("1. Agregar Producto");
+            System.out.println("2. Eliminar Producto");
+            System.out.println("3. Modificar Stock");
+            System.out.println("4. Mostrar inventario");
+            System.out.println("5. Salir");
+            System.out.println("Selecciona una opcion: ");
+
+            int opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch(opcion){
+                case 1:
+                    System.out.print("Ingresar ID (7 Caracteres): ");
+                    String id = teclado.nextLine();
+
+                    System.out.print("Ingrese Nombre del producto: ");
+                    String nombre = teclado.nextLine();
+
+                    System.out.print("Ingrese Precio: ");
+                    double precio = teclado.nextDouble();
+
+                    System.out.print("Ingrese su stock: ");
+                    int stock = teclado.nextInt();
+                    teclado.nextLine();
+
+                    MatiasMetodos.AgregarProducto(baseDeDatos, id, nombre, precio, stock);
+                    break;
+                case 2:
+                    System.out.println("Ingrese ID a eliminar: ");
+                    String idEliminar = teclado.nextLine();
+
+                    MatiasMetodos.EliminarProducto(baseDeDatos, idEliminar);
+                    break;
+
+            }
+
+        }
     }
 }
