1be1adb2148d71c631ebe201553e77eb4a6fa7b0
jhonatan050 <jhonatanchango9@gmail.com>
2026-05-26 19:52:42 -0500

    Documentacion de la clase validadorUsuarios

--- src/JhonatanMetodos/ValidadorUsuarios.java
+++ src/JhonatanMetodos/ValidadorUsuarios.java
@@ -1,8 +1,12 @@
 package JhonatanMetodos;
-
+/**
+ * Clase encargada de la validacion de los usuarios
+ * El usuario debe tener una estructura fija: nombre.cliente || nombre.usuario
+ * @author Denis Jhonatan Chango Cutiopala
+ * @version 1.1
+ */
 public class ValidadorUsuarios {
 
-    // Tus constantes basadas exactamente en tu grafo en papel
     public final int er = -10;
     public final int ESTADO_ADMIN = 14;   // q14
     public final int ESTADO_CLIENTE = 9;  // q9 
@@ -10,23 +14,27 @@ public class ValidadorUsuarios {
     // Matriz 
     final int mt[][] = {
         // .   c   l   i   e   n   t   a   d   m  Otro Enter
-        { er,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  er }, // q0: Cualquier letra/número arranca el nombre -> q1
-        {  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  er }, // q1: El nombre puede contener letras del alfabeto. '.' va a q2
-        { er,  3, er, er, er, er, er, 10, er, er, er,  er }, // q2: Bifurcación ('c' -> q3, 'a' -> q10)
-        { er, er,  4, er, er, er, er, er, er, er, er,  er }, // q3: 'l' -> q4
-        { er, er, er,  5, er, er, er, er, er, er, er,  er }, // q4: 'i' -> q5
-        { er, er, er, er,  6, er, er, er, er, er, er,  er }, // q5: 'e' -> q6
-        { er, er, er, er, er,  7, er, er, er, er, er,  er }, // q6: 'n' -> q7
-        { er, er, er, er, er, er,  8, er, er, er, er,  er }, // q7: 't' -> q8
-        { er, er, er, er,  9, er, er, er, er, er, er,  er }, // q8: 'e' -> q9
-        { er, er, er, er, er, er, er, er, er, er, er,   9 }, // q9: ESTADO DE ACEPTACIÓN (Consume el Enter)
-        { er, er, er, er, er, er, er, er, 11, er, er,  er }, // q10: 'd' -> q11
-        { er, er, er, er, er, er, er, er, er, 12, er,  er }, // q11: 'm' -> q12
-        { er, er, er, 13, er, er, er, er, er, er, er,  er }, // q12: 'i' -> q13
-        { er, er, er, er, er, 14, er, er, er, er, er,  er }, // q13: 'n' -> q14
-        { er, er, er, er, er, er, er, er, er, er, er,  14 }  // q14: ESTADO DE ACEPTACIÓN (Consume el Enter)
+        { er,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  er }, // q0
+        {  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  er }, // q1 
+        { er,  3, er, er, er, er, er, 10, er, er, er,  er }, // q2 
+        { er, er,  4, er, er, er, er, er, er, er, er,  er }, // q3 
+        { er, er, er,  5, er, er, er, er, er, er, er,  er }, // q4 
+        { er, er, er, er,  6, er, er, er, er, er, er,  er }, // q5 
+        { er, er, er, er, er,  7, er, er, er, er, er,  er }, // q6 
+        { er, er, er, er, er, er,  8, er, er, er, er,  er }, // q7 
+        { er, er, er, er,  9, er, er, er, er, er, er,  er }, // q8 
+        { er, er, er, er, er, er, er, er, er, er, er,   9 }, // q9 
+        { er, er, er, er, er, er, er, er, 11, er, er,  er }, // q10 
+        { er, er, er, er, er, er, er, er, er, 12, er,  er }, // q11 
+        { er, er, er, 13, er, er, er, er, er, er, er,  er }, // q12 
+        { er, er, er, er, er, 14, er, er, er, er, er,  er }, // q13 
+        { er, er, er, er, er, er, er, er, er, er, er,  14 }  // q14 
     };
-
+    /**
+     * Nos dirije a la columna correspondiente en la matriz de transicion.
+     * @param c: Evalua a que categoria pertenece el caracter y retorna el valor de la columna correspondiente.
+     * @return: en caso de ser un caracter invalido retorna er correspondiente a la variable de error.
+     */
     private int getIndexAlfabeto(char c) {
         switch (c) {
             case '.': return 0;
@@ -45,7 +53,12 @@ public class ValidadorUsuarios {
                 return er; 
         }
     }
-
+    /**
+     * validarUsuario se encarga de recorrer la matriz de transicion comprobando caracter a caracter
+     * si el usuario es valido o no.
+     * @param nombreIngresado: Es el usuario que se va a comprobar.
+     * @return: una vez recorrida toda la matriz de transicion nos retorna el estado q9 || q14 los cuales son los estados de aceptacion. 
+     */
     public int validarUsuario(String nombreIngresado) {
         int indexAlfa = 0;
         int q = 0; 
